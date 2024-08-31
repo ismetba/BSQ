e7413d46b0979496150471f068577a1a1a3ca806
Yasin Zeybek <yzeybek@k1m17s01.42istanbul.com.tr>
2024-08-31 17:13:10 +0300

    keeping split_content algorithm

--- srcs/split_content.c
+++ srcs/split_content.c
@@ -6,7 +6,7 @@
 /*   By: yzeybek <yzeybek@student.42istanbul.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/31 12:50:12 by yzeybek           #+#    #+#             */
-/*   Updated: 2024/08/31 15:19:01 by yzeybek          ###   ########.fr       */
+/*   Updated: 2024/08/31 17:05:56 by yzeybek          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,21 +31,6 @@ int	ft_atoi_light(char *first_line, struct s_map *map)
 	return (i);
 }
 
-int	get_chars(char *first_line, int i, struct s_map *map)
-{
-	if (first_line[i] <= 126 && first_line[i] >= 32
-		&& first_line[i + 1] <= 126 && first_line[i + 1] >= 32
-		&& first_line[i + 2] <= 126 && first_line[i + 2] >= 32)
-	{
-		(*map).empty = first_line[i];
-		(*map).obstacle = first_line[i + 1];
-		(*map).full = first_line[i + 2];
-		return (1);
-	}
-	else
-		return (0);
-}
-
 int	split_first(char *content, struct s_map *map, int i)
 {
 	char	*first_line;
@@ -61,9 +46,45 @@ int	split_first(char *content, struct s_map *map, int i)
 	nbr_length = ft_atoi_light(first_line, map);
 	if (!nbr_length)
 		return (0);
-	if (!get_chars(first_line, nbr_length, map))
+	if (first_line[i] <= 126 && first_line[i] >= 32
+		&& first_line[i + 1] <= 126 && first_line[i + 1] >= 32
+		&& first_line[i + 2] <= 126 && first_line[i + 2] >= 32)
+	{
+		(*map).empty = first_line[i];
+		(*map).obstacle = first_line[i + 1];
+		(*map).full = first_line[i + 2];
+	}
+	else
 		return (0);
 	return (1);
+}
+
+int	split_lines(char *content, struct s_map *map)
+{
+	int		i;
+	int		j;
+	char	*line;
+	int		line_length;
+	char	**map_data;
+
+	i = 0;
+	map_data = (char **)malloc(sizeof(char *) * (*map).line_count)
+	line_length = ft_strlen(content);
+	while (i <= (*map).line_count)
+	{
+		line = (char *)malloc(sizeof(char) * (j + 1));
+		j = 0;
+		while (content[j] != '\n')
+		{
+			if (line_length == -1)
+				line[j] = content[j];
+			else
+				if (j == )
+			j++;
+		}
+		line[j] = '\0';
+		i++;
+	}
 }
 
 int	split_content(char *content, struct s_map *map)
@@ -76,6 +97,8 @@ int	split_content(char *content, struct s_map *map)
 	if (!check_max_length(i))
 		return (0);
 	if (!split_first(content, map, i))
+		return (0);
+	if (!split_lines(content, map))
 		return (0);
 //FUNCTION WILL KEEP SPLITING AND CHEKING OTHER LINES OF MAP
 	return (1);
